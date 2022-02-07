9bcff11784719b2311ba96ab29fcd8efb6dac103
rajeshmn47 <rajeshmn47@gmail.com>
2022-02-07 08:41:34 +0530

    second commit

--- src/components/Connections.js
+++ src/components/Connections.js
@@ -2,9 +2,12 @@ import {useState,useEffect} from 'react'
 import axios from 'axios'
 
 export const Connections=()=>{
-useEffect(async ()=>{
-const a=await axios.get('')
-})
+const [users,setUsers]=useState([])
+useEffect(async() => {
+  
+  const g=await axios.get("http://127.0.0.1:3001/auth/getusers");
+  setUsers(g)
+     }, []);
     return(
         <h1>friends list</h1>
     )
