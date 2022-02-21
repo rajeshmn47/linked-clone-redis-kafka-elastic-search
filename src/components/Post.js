b98142b586934fe38e826fa5db7e0a7e64237703
rajeshmn47 <rajeshmn47@gmail.com>
2022-02-21 21:33:05 +0530

    second commit

--- src/components/Post.js
+++ src/components/Post.js
@@ -10,26 +10,74 @@ import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
 import CommentOutlinedIcon from '@material-ui/icons/CommentOutlined';
 import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
 import Usercars from './Usercars'
+import LongMenu from './Menu'
+import {useSelector} from 'react-redux'
+import { useEffect,useState} from "react";
+import Collapse from '@material-ui/core/Collapse';
+import {TextField} from '@material-ui/core'
+ 
+// format timestamp
 
 
 export const Post=({p})=>{
-    console.log(p)
+    const {user}= useSelector((state) => state.user);
+    const t=p.likes.includes(user?._id)
+    console.log(t)
+    console.log(p.likes.includes(user?._id))
+    const[isliked,setIsliked]=useState(t)
+    const[expanded,setExpanded]=useState(false)
+    const[likes,setLikes]=useState(p.likes.length)
+    console.log(p.comments,'raju')
+    useEffect(() => {
+        setIsliked(p.likes.includes(user?._id));
+      }, [user, p.likes]);
+  const commenthandler=()=>{
+setExpanded(!expanded)
+  }  
+ 
+    const likehandler=async()=>{
+        setIsliked(!isliked)
+        if(isliked){
+        setLikes(likes-1)
+        }
+        else{
+            setLikes(likes+1)
+        }
+        console.log('rajesh')
+await axios.post('http://127.0.0.1:3001/auth/likehandler',{userid:user._id,postid:p._id})
+    }
+
     return(
         <>
+        
    {p.userId&&<Usercars post={p} />}
-    <p>{p.text}</p>
+ 
+    <p>{p.desc}</p>
 {p.img&&<img src={`http://127.0.0.1:3001/images/${p.img}`}  height='200'/>}
 <div className='icons'>
-    <div className='icon'>
-<ThumbUpAltOutlinedIcon htmlColor='rgb(138, 132, 132)'/>Like
+ {isliked? <div className='icon' onClick={()=>likehandler()}>
+<ThumbUpAltOutlinedIcon htmlColor='green'/>likes({likes})
 </div>
-<div className='icon'>
-<CommentOutlinedIcon htmlColor='rgb(138, 132, 132)'/>Comment
+:
+<div className='icon' onClick={()=>likehandler()}>
+    <ThumbUpAltOutlinedIcon htmlColor='rgb(138, 132, 132)' />likes({likes})
+    </div>
+}
+<div className='icon' onClick={()=>commenthandler()}>
+<CommentOutlinedIcon htmlColor='rgb(138, 132, 132)'/>Comments({p.comments.length})
 </div>
 <div className='icon'>
 <ShareOutlinedIcon htmlColor='rgb(138, 132, 132)'/>Share
 </div>
 </div>
+<div style={{zIndex:'100',marginTop:'1vmax'}}>
+    <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <TextField style={{width:'40vw'}} variant='outlined' placeholder='comment here'/>
+        {p.comments.map((t)=><p style={{marginTop:'1vmax'}}>{t.text}</p>
+        )}       
+        </Collapse>
+</div>
+
 </>
     )
 }
--- src/components/Usercars.js
+++ src/components/Usercars.js
@@ -1,8 +1,13 @@
 import axios from 'axios'
 import { useEffect,useState } from 'react'
+import {format} from 'timeago.js'
+import LongMenu from './Menu'
+import {useSelector} from 'react-redux'
 
 export const Usercars=({post})=>{
+    const {user}= useSelector((state) => state.user);
   const[use,setUse]=useState()
+  console.log(post)
 useEffect(async()=>{
 const r=await axios.get(`http://127.0.0.1:3001/auth/getuser/${post.userId}`)
 console.log(r,'kkkk')
@@ -11,12 +16,18 @@ setUse(r.data.user)
     return(
        <>
        <div className='usercardd'>
-           {post.timestamp}
+          
        <img src={use?.profilePicture?`http://127.0.0.1:3001/images/${use.profilePicture}`:`http://127.0.0.1:3001/images/noprofile.jpeg`} 
        style={{borderRadius:'50%',marginRight:'1vmax'}}height='30' width='30'/>
-        {use?.first_name}
-    
+   
+        <div style={{display:'block'}}>
+        {use?.first_name}</div>
+        <span style={{marginLeft:'35vw',display:'flex',fontSize:'1vmax'}}>
+          {format(post.createdAt)}
+          {user&&post&&user._id===post.userId?<LongMenu post={post} />:null}
+          </span>        
         </div>
+    
         </>
     )
 }
